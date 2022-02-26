66ae1a46062dee07063d5ff24bad46fbd7cf6336
209 <209@academicos.cesde.edu.co>
2022-02-26 16:31:59 -0500

    trabajo clase sabado

---
+++ app.js
@@ -0,0 +1,22 @@
+// funcion tradicional calcular la distancia entre dos puntos 
+
+function calcularDistancia(x1,y1,x2,y2){
+
+
+    return (Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)))
+    
+}
+
+
+let distancia=(calcularDistancia(0,0,-10,-10)).toFixed(2)
+console.log (`la distancia es ${distancia}`)
+
+let calcularDistanciaflecha = (x1,y1,x2,y2)=> Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)).toFixed(2)
+
+console.log(`la distancia es ${calcularDistanciaflecha(0,0,-10,-10)}`)
+
+
+
+
+
+
